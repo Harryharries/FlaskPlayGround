5c0bfb25b7bb643b53fbd7a571e55584ca2ee7b0
“Harryharries” <harry.guapi@gmail.com>
2021-11-16 17:08:41 -0600

    code refactor: bule print

---
+++ flaskblog/config.py
@@ -0,0 +1,11 @@
+import os
+
+
+class Config:
+    SECRET_KEY = '99702983e5f0ccc6f5481d6ee634f2d1'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
--- flaskblog/users/forms.py
+++ flaskblog/users/forms.py
@@ -6,6 +6,7 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField,TextAr
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 
 
+
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
                            validators=[DataRequired(), Length(min=2, max=20)])
@@ -55,10 +56,6 @@ class UpdateAccountForm(FlaskForm):
             if user:
                 raise ValidationError("email exist already")
 
-class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    content = TextAreaField('Content', validators=[DataRequired()])
-    submit = SubmitField('Post')
 
 
 class RequestResetForm(FlaskForm):
